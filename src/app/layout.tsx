868bae7ba39f76f8b4e02677c51bc2c6a38967ce
jo <jyothishkpk@gmail.com>
2025-06-19 13:53:14 +0530

    ytytyt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="ba-wrapper">
         {children}
+        </div>
       </body>
     </html>
   );
