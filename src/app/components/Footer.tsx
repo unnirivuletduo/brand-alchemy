fc8a9fb8de90798eda9d2b321ae5265984eb45b4
jo <jyothishkpk@gmail.com>
2025-10-07 09:31:53 +0530

    iopo

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -26,9 +26,9 @@ const XIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
     fill="#99a1af"
     className="bi bi-twitter-x"
-    viewBox="0 0 16 16"
-    height="16"
-    width="16"
+    viewBox="0 0 13 13"
+    height="13"
+    width="13"
   >
     <path
       d="M12.6 0.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867 -5.07 -4.425 5.07H0.316l5.733 -6.57L0 0.75h5.063l3.495 4.633L12.601 0.75Zm-0.86 13.028h1.36L4.323 2.145H2.865z"
